689303d7e01e7213b80efbeb1a14004e13566e80
Guillaume Meunier <guillaume.meunier@centraliens.net>
2015-10-20 16:59:12 +0200

    Added debug utils

---
+++ Common/DebugUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProceduralCities
+{
+	public static class Utils
+	{
+		[System.Diagnostics.Conditional("DEBUG")]
+		static protected void Assert(bool condition)
+		{
+			if (!condition)
+				throw new Exception("Assertion failure");
+		}
+	}
+}
+
