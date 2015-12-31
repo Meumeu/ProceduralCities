02bfc76070d352f123c15cf6a89310fbd6c891d2
Guillaume Meunier <guillaume.meunier@centraliens.net>
2015-12-31 15:09:39 +0100

    Beginning of a street network inside a city (PoC)

--- TestStreetNetwork/StreetNetwork.cs
+++ TestStreetNetwork/StreetNetwork.cs
@@ -1,37 +1,66 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using Cairo;
 
 namespace ProceduralCities
 {
-	class TestStreetNetwork
+	class StreetNodeList
 	{
-		public static void Main(string[] args)
-		{
-			var net = new StreetNetwork();
-//			net.DrawPopulation("Population.png");
-//			net.DrawHeightMap("Map.png");
-//			net.DrawRoadTensor("RoadTensor.png");
-
-//			for (double x = 0.1; x < 2.5; x += 0.2)
-//			{
-//				for (double y = 0.1; y < 2.5; y += 0.2)
-//				{
-//					net.BuildRoad(x, y, true);
-//				}
-//			}
-
-//			net.DrawRoadNetwork("Roads.png");
-		}
 	}
 
-	struct StreetNode
+	class StreetNode
 	{
 		public double x;
 		public double y;
+
+		public StreetNode(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public StreetNode()
+		{
+		}
+
 		public override string ToString()
 		{
 			return String.Format("StreetNode({0:F3}, {1:F3})", x, y);
+		}
+	}
+
+	class StreetSegment
+	{
+		public StreetNode node1;
+		public StreetNode node2;
+		public int generation;
+		public bool stop;
+
+		public override string ToString()
+		{
+			return String.Format("StreetSegment({0}-{1})", node1, node2);
+		}
+	}
+
+	class StreetNetwork
+	{
+		//List<StreetNode> nodes = new List<StreetNode>();
+		List<StreetSegment> segments = new List<StreetSegment>();
+		SortedDictionary<int, Queue<StreetSegment>> candidates = new SortedDictionary<int, Queue<StreetSegment>>();
+
+		public static double Distance(StreetSegment s, StreetNode n)
+		{
+			StreetNode n1 = s.node1;
+			StreetNode n2 = s.node2;
+			double lambda = ((n.x - n1.x) * (n2.x - n1.x) + (n.y - n1.y) * (n2.y - n1.y)) / ((n2.x - n1.x) * (n2.x - n1.x) + (n2.y - n1.y) * (n2.y - n1.y));
+
+			if (lambda < 0)
+				lambda = 0;
+			else if (lambda > 1)
+				lambda = 1;
+
+			return Distance(n, n1.x + lambda * (n2.x - n1.x), n1.y + lambda * (n2.y - n1.y));
 		}
 
 		public static double Distance(StreetNode n1, double x, double y)
@@ -43,58 +72,172 @@ namespace ProceduralCities
 		{
 			return Math.Sqrt((n1.x - n2.x) * (n1.x - n2.x) + (n1.y - n2.y) * (n1.y - n2.y));
 		}
-	}
 
-	struct StreetSegment
-	{
-		public int node1;
-		public int node2;
-		public int generation;
-	}
-
-	class StreetNetwork
-	{
-		List<StreetNode> nodes = new List<StreetNode>();
-		List<StreetSegment> streets = new List<StreetSegment>();
-
-		public double Distance(StreetSegment s, StreetNode n)
+		public bool Intersects(StreetSegment s1, StreetSegment s2, out double lambda, out double mu)
 		{
-			StreetNode n1 = nodes[s.node1];
-			StreetNode n2 = nodes[s.node2];
-			double lambda = ((n.x - n1.x) * (n2.x - n1.x) + (n.y - n1.y) * (n2.y - n1.y)) / ((n2.x - n1.x) * (n2.x - n1.x) + (n2.y - n1.y) * (n2.y - n1.y));
+			// s1: [n1 n2]
+			// s2: [n3 n4]
+			// intersection = n1 + lambda * (n2 - n1) = n3 + mu * (n4 - n3)
+			// lambda * (n2.x - n1.x) + mu * (n3.x - n4.x) = n3.x - n1.x
+			// lambda * (n2.y - n1.y) + mu * (n3.y - n4.y) = n3.y - n1.y
 
-			if (lambda < 0)
-				lambda = 0;
-			else if (lambda > 1)
-				lambda = 1;
+			StreetNode n1 = s1.node1;
+			StreetNode n2 = s1.node2;
+			StreetNode n3 = s2.node1;
+			StreetNode n4 = s2.node2;
 
-			return StreetNode.Distance(n, n1.x + lambda * (n2.x - n1.x), n1.y + lambda * (n2.y - n1.y));
+			double det = (n2.x - n1.x) * (n3.y - n4.y) - (n2.y - n1.y) * (n3.x - n4.x);
+			lambda = ((n3.x - n1.x) * (n3.y - n4.y) - (n3.y - n1.y) * (n3.x - n4.x)) / det;
+			mu = ((n2.x - n1.x) * (n3.y - n1.y) - (n2.y - n1.y) * (n3.x - n1.x)) / det;
+
+			return (lambda >= 0 && lambda <= 1 && mu >= 0 && mu <= 1);
 		}
 
-		public int FindClosestNode(double x, double y)
+		public StreetNode FindClosestNode(double x, double y)
 		{
-			int best = -1;
+			StreetNode best = null;
 			double bestDistance = double.MaxValue;
 
-			for (int i = 0; i < nodes.Count; i++)
+			foreach(StreetSegment i in segments)
 			{
-				double distance = Distance(nodes[i], x, y);
+				double distance = Distance(i.node1, x, y);
 				if (distance < bestDistance)
 				{
 					bestDistance = distance;
-					best = i;
+					best = i.node1;
+				}
+
+				distance = Distance(i.node2, x, y);
+				if (distance < bestDistance)
+				{
+					bestDistance = distance;
+					best = i.node2;
 				}
 			}
 
 			return best;
 		}
 
-		public int FindClosestNode(StreetNode node)
+		public StreetNode FindClosestNode(StreetNode node)
 		{
 			return FindClosestNode(node.x, node.y);
 		}
 
-		double scale = 0.003;
+		StreetNode FindOrCreateNode(double x, double y, double tol)
+		{
+			StreetNode node = FindClosestNode(x, y);
+
+			if (node != null && Distance(node, x, y) < tol)
+			{
+				return node;
+			}
+			else
+			{
+				StreetNode n = new StreetNode();
+				n.x = x;
+				n.y = y;
+				return n;
+			}
+		}
+
+		void SplitSegment(StreetSegment s, double lambda, double tol)
+		{
+			StreetNode middle = FindOrCreateNode(
+				                    s.node1.x + lambda * (s.node2.x - s.node1.x),
+				                    s.node1.y + lambda * (s.node2.y - s.node1.y),
+				                    tol);
+			StreetSegment newseg = new StreetSegment();
+			newseg.node1 = middle;
+			newseg.node2 = s.node2;
+			newseg.generation = s.generation;
+			segments.Add(newseg);
+
+			s.node2 = middle;
+		}
+
+		// Modify the segment and returns true if the segment should be kept
+		bool LocalConstraints(ref StreetSegment segment)
+		{
+			if (segment.generation > 4)
+				return false;
+
+			double intersecting_lambda = 0;
+			double intersecting_mu = 1;
+			int intersecting_segment = -1;
+			for(int i = 0, n = segments.Count; i < n; i++)
+			{
+				double lambda, mu;
+				if (Intersects(segments[i], segment, out lambda, out mu))
+				{
+					if (mu < intersecting_mu && mu > 0.0001)
+					{
+						intersecting_segment = i;
+						intersecting_lambda = lambda;
+						intersecting_mu = mu;
+					}
+				}
+			}
+
+			if (intersecting_segment >= 0)
+			{
+				SplitSegment(segments[intersecting_segment], intersecting_lambda, 0.1);
+				segment.node2 = FindOrCreateNode(
+					segment.node1.x + (segment.node2.x - segment.node1.x) * intersecting_mu,
+					segment.node1.y + (segment.node2.y - segment.node1.y) * intersecting_mu,
+					0.1);
+				segment.stop = true;
+			}
+
+			StreetNode closest = FindClosestNode(segment.node2);
+			if (Distance(segment.node2, closest) < 5)
+			{
+				segment.node2 = closest;
+				segment.stop = true;
+			}
+
+			return Distance(segment.node1, segment.node2) > 5
+				&& Distance(segment.node1, new StreetNode(0,0)) < 1000
+				&& Distance(segment.node2, new StreetNode(0,0)) < 1000;
+		}
+
+		Random rnd = new Random();
+		// Add candidate segments
+		void GlobalGoals(StreetSegment segment, int t)
+		{
+			if (segment.stop)
+				return;
+
+			double x = segment.node2.x;
+			double y = segment.node2.y;
+			double theta = (rnd.NextDouble() - 0.5) * 0.4;
+			double dx = (x - segment.node1.x) * Math.Cos(theta) - (y - segment.node1.y) * Math.Sin(theta);
+			double dy = (x - segment.node1.x) * Math.Sin(theta) + (y - segment.node1.y) * Math.Cos(theta);
+
+			// Continue the current road
+			StreetSegment seg = new StreetSegment();
+			seg.node1 = segment.node2;
+			seg.node2 = FindOrCreateNode(x + dx, y + dy, 0.1);
+			seg.generation = segment.generation;
+			seg.stop = false;
+			PushCandidate(t + 1, seg);
+
+			// Branch secondary roads
+			seg = new StreetSegment();
+			seg.node1 = segment.node2;
+			seg.node2 = FindOrCreateNode(x + dy, y - dx, 0.1);
+			seg.generation = segment.generation + 1;
+			seg.stop = false;
+			PushCandidate(t + 1, seg);
+
+			seg = new StreetSegment();
+			seg.node1 = segment.node2;
+			seg.node2 = FindOrCreateNode(x - dy, y + dx, 0.1);
+			seg.generation = segment.generation + 1;
+			seg.stop = false;
+			PushCandidate(t + 1, seg);
+		}
+
+		double scale = 0.3;
 		int width = 800;
 		int height = 800;
 
@@ -102,10 +245,58 @@ namespace ProceduralCities
 		{
 		}
 
-//		public void Add
+		bool PopCandidate(out StreetSegment segment, out int t)
+		{
+			while (candidates.Count > 0)
+			{
+				var tmp = candidates.First(); // TODO: vérifier si O(log n) ?
+				if (tmp.Value.Count > 0)
+				{
+					t = tmp.Key;
+					segment = tmp.Value.Dequeue();
+					return true;
+				}
+
+				candidates.Remove(tmp.Key);
+			}
+
+			t = 0;
+			segment = null;
+			return false;
+		}
+
+		void PushCandidate(int t, StreetSegment segment)
+		{
+			if (!candidates.ContainsKey(t))
+				candidates.Add(t, new Queue<StreetSegment>());
+
+			candidates[t].Enqueue(segment);
+		}
+
+		public void AddRoad(double x1, double y1, double x2, double y2)
+		{
+			StreetSegment seg = new StreetSegment();
+			seg.node1 = FindOrCreateNode(x1, y1, 1);
+			seg.node2 = FindOrCreateNode(x2, y2, 1);
+			seg.generation = 0;
+			seg.stop = false;
+
+			segments.Add(seg);
+			GlobalGoals(seg, 0);
+		}
 
 		public void BuildRoads()
 		{
+			StreetSegment seg;
+			int t;
+			while (PopCandidate(out seg, out t) && segments.Count < 1000)
+			{
+				if (LocalConstraints(ref seg))
+				{
+					segments.Add(seg);
+					GlobalGoals(seg, t);
+				}
+			}
 		}
 
 		/*public void DrawPopulation(string filename)
@@ -171,8 +362,9 @@ namespace ProceduralCities
 			}
 		}*/
 
-		/*public void DrawRoadTensor(string filename)
+		public void DrawRoadNetwork(string filename)
 		{
+			Console.WriteLine("{0} segments", segments.Count);
 			using (var surface = new ImageSurface(Format.RGB24, width, height))
 			{
 				using (var ctx = new Context(surface))
@@ -184,164 +376,65 @@ namespace ProceduralCities
 					ctx.LineTo(0, height);
 					ctx.Fill();
 
-					ctx.SetSourceColor(new Color(0, 0, 0));
 					ctx.LineWidth = 2;
-					for (int i = 20; i < width; i += 40)
+					foreach (StreetSegment s in segments)
 					{
-						for (int j = 20; j < height; j += 40)
+						switch (s.generation)
 						{
-							double x, y;
-							GetTensor(i * scale, j * scale, out x, out y);
-							double theta = Math.Atan2(y, x) / 2;
+						case 0:
+							ctx.SetSourceColor(new Color(1, 0, 0));
+							break;
 
-							ctx.MoveTo(i, j);
-							ctx.RelLineTo(20 * Math.Cos(theta), 20 * Math.Sin(theta));
-							ctx.RelLineTo(5 * Math.Cos(theta + 3 * Math.PI / 4), 5 * Math.Sin(theta + 3 * Math.PI / 4));
-							ctx.Stroke();
+						case 1:
+							ctx.SetSourceColor(new Color(0, 0, 1));
+							break;
 
-							ctx.MoveTo(i + 20 * Math.Cos(theta), j + 20 * Math.Sin(theta));
-							ctx.RelLineTo(5 * Math.Cos(theta - 3 * Math.PI / 4), 5 * Math.Sin(theta - 3 * Math.PI / 4));
-							ctx.Stroke();
+						case 2:
+							ctx.SetSourceColor(new Color(0, 1, 0));
+							break;
+
+						default:
+							ctx.SetSourceColor(new Color(1, 1, 0));
+							break;
+
 						}
+
+						StreetNode n1 = s.node1;
+						StreetNode n2 = s.node2;
+						ctx.MoveTo(n1.x / scale + width / 2, n1.y / scale + height / 2);
+						ctx.LineTo(n2.x / scale + width / 2, n2.y / scale + height / 2);
+						ctx.Stroke();
 					}
 				}
 				surface.WriteToPng(filename);
 			}
-		}*/
+		}
+	}
 
-		/*public void DrawRoadNetwork(string filename)
+	class TestStreetNetwork
+	{
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("{0} nodes, {1} segments", nodes.Count, streets.Count);
-			using (var surface = new ImageSurface(Format.RGB24, width, height))
-			{
-				using (var ctx = new Context(surface))
-				{
-					ctx.SetSourceColor(new Color(1, 1, 1));
-					ctx.MoveTo(0, 0);
-					ctx.LineTo(width, 0);
-					ctx.LineTo(width, height);
-					ctx.LineTo(0, height);
-					ctx.Fill();
+			var net = new StreetNetwork();
+			net.AddRoad(0, 0, 0, 10);
+			net.AddRoad(0, 0, 0, -10);
+			net.AddRoad(0, 0, 10, 0);
+			net.AddRoad(0, 0, -10, 0);
+			net.BuildRoads();
+			net.DrawRoadNetwork("Roads.png");
+			//			net.DrawPopulation("Population.png");
+			//			net.DrawHeightMap("Map.png");
+			//			net.DrawRoadTensor("RoadTensor.png");
 
-					ctx.SetSourceColor(new Color(1, 1, 0));
-					ctx.LineWidth = 2;
-					foreach (Street s in streets)
-					{
-						if (!s.major)
-						{
-							StreetNode n1 = nodes[s.node1];
-							StreetNode n2 = nodes[s.node2];
-							ctx.MoveTo(n1.x / scale, n1.y / scale);
-							ctx.LineTo(n2.x / scale, n2.y / scale);
-						}
-					}
-					ctx.Stroke();
+			//			for (double x = 0.1; x < 2.5; x += 0.2)
+			//			{
+			//				for (double y = 0.1; y < 2.5; y += 0.2)
+			//				{
+			//					net.BuildRoad(x, y, true);
+			//				}
+			//			}
 
-					ctx.SetSourceColor(new Color(1, 0, 0));
-					ctx.LineWidth = 3;
-					foreach (Street s in streets)
-					{
-						if (s.major)
-						{
-							StreetNode n1 = nodes[s.node1];
-							StreetNode n2 = nodes[s.node2];
-							ctx.MoveTo(n1.x / scale, n1.y / scale);
-							ctx.LineTo(n2.x / scale, n2.y / scale);
-						}
-					}
-					ctx.Stroke();
-				}
-				surface.WriteToPng(filename);
-			}
-		}*/
-
-		/*public StreetNode NextStreetNode(StreetNode node, bool major)
-		{
-			double x1, y1;
-
-			double Tx, Ty;
-			GetTensor(node.x, node.y, out Tx, out Ty);
-			double theta = Math.Atan2(Ty, Tx) / 2 + (major ? 0 : Math.PI / 2);
-			double dx1 = Math.Cos(theta);
-			double dy1 = Math.Sin(theta);
-
-			x1 = node.x + dx1;
-			y1 = node.y + dy1;
-
-			GetTensor(x1, y1, out Tx, out Ty);
-			theta = Math.Atan2(Ty, Tx) / 2 + (major ? 0 : Math.PI / 2);
-			double dx2 = Math.Cos(theta);
-			double dy2 = Math.Sin(theta);
-
-			StreetNode ret = new StreetNode();
-			double dx = (dx1 + dx2) / 2;
-			double dy = (dy1 + dy2) / 2;
-			double d = Math.Sqrt(dx * dx + dy * dy);
-
-
-			ret.x = node.x + 0.05*dx/d;
-			ret.y = node.y + 0.05*dy/d;
-
-			return ret;
-		}*/
-
-		/*public void BuildRoad(double x0, double y0, bool major)
-		{
-			int idx = FindClosestNode(x0, y0);
-
-			if (idx == -1 || Distance(nodes[idx], x0, y0) > 0.01)
-			{
-				idx = nodes.Count;
-				StreetNode node = new StreetNode();
-				node.x = x0;
-				node.y = y0;
-				nodes.Add(node);
-				Console.WriteLine("Starting road at new node {0}", node);
-			}
-			else
-			{
-				Console.WriteLine("Starting road at existing node {0}", nodes[idx]);
-			}
-
-			BuildRoad(idx, major);
-		}*/
-
-		/*public void BuildRoad(int initialNode, bool major)
-		{
-			Street s;
-			while (true)
-			{
-				StreetNode next = NextStreetNode(nodes[initialNode], major);
-				if (next.x < 0 || next.x > width * scale)
-					return;
-				
-				if (next.y < 0 || next.y > height * scale)
-					return;
-
-				int closestNode = FindClosestNode(next);
-				if (closestNode >= 0 && Distance(next, nodes[closestNode]) < 0.04)
-				{
-					s = new Street();
-					s.node1 = initialNode;
-					s.node2 = closestNode;
-					s.major = major;
-					streets.Add(s);
-					Console.WriteLine("Finished on existing node {0}", next);
-					return;
-				}
-
-				int newNodeIdx = nodes.Count;
-				nodes.Add(next);
-				Console.WriteLine("Added new node {0}", next);
-
-				s = new Street();
-				s.node1 = initialNode;
-				s.node2 = newNodeIdx;
-				s.major = major;
-				streets.Add(s);
-
-				initialNode = newNodeIdx;
-			}
-		}*/
+			//			net.DrawRoadNetwork("Roads.png");
+		}
 	}
 }
